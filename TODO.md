4f54d945a2904d1a787f968fd24c3208b706eacd
CorcovadoMing <rf37535@gmail.com>
2016-02-24 20:40:04 +0800

    Publish beta

---
+++ TODO.md
@@ -0,0 +1,17 @@
+### High
+
+- Documents generator deal with markdown to html
+- Show the drone information
+
+### Medium
+
+- Set up the secure login
+- Add sensors simulate
+- Allow add wifi signal or obstacle by users
+- Plotting the information
+- Debugger
+
+### Low
+
+- Other language supported
+- Layout refining
---
+++ assets/js/code.js
@@ -0,0 +1,56 @@
+default_algorithm = `// "init" call once the page loaded, "main" will be called every 1/60 second
+
+init = function() {
+    env.token = 0;
+    env.last_rssi;
+    env.last_move; // 0 for left, 1 for right
+    env.in_region = 0;
+}
+
+
+main = function() {
+    env.token += 1;
+    env.token = env.token % 15;
+    if (env.token === 0) {
+    
+        if (drone.rssi > 0) {
+            drone.acc = 100;
+            if (env.in_region === 0) {
+	        if (Math.random() > 0.5) {
+                    drone.turn_left(5);
+                    env.last_move = 0;
+                }
+	        else {
+                    drone.turn_right(5);
+                    env.last_move = 1;
+                }
+                env.in_region = 1;
+            }
+            else {
+                if (drone.rssi > env.last_rssi) {
+                    // do nothing, keep going
+                }
+                else {
+                    if (env.last_move === 0) drone.turn_left(180-10*Math.random());
+                    else drone.turn_right(180-10*Math.random());
+                }
+            }
+            env.last_rssi = drone.rssi;
+        }
+
+        else {
+            env.in_region = 0;
+            drone.acc = 200;
+                if (Math.random() > 0.5)
+                    drone.turn_left(Math.random()*50);
+	        else
+                    drone.turn_right(Math.random()*50);
+        }
+    
+    }
+    else {
+        drone.acc = 0;
+    }
+}
+`
+
