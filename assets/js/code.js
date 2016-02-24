f6e0f92dedc927dd27fe22001e1541cff79bf360
CorcovadoMing <rf37535@gmail.com>
2016-02-24 20:14:44 +0800

    Fix the program

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,42 +1,48 @@
-default_algorithm = `var token = 0;
-var last_rssi;
-var last_move; // 0 for left, 1 for right
-var in_region = 0;
+default_algorithm = `// "init" call once the page loaded, "main" will be called every 1/60 second
+
+var env = {}
+
+var init = function() {
+    env.token = 0;
+    env.last_rssi;
+    env.last_move; // 0 for left, 1 for right
+    env.in_region = 0;
+}
 
 
-main = function() {
+var main = function() {
 
-    token += 1;
-    token = token % 15;
-    if (token === 0) {
+    env.token += 1;
+    env.token = envtoken % 15;
+    if (env.token === 0) {
     
         if (drone.rssi > 0) {
             drone.acc = 100;
-            if (in_region === 0) {
+            if (env.in_region === 0) {
 	        if (Math.random() > 0.5) {
                     drone.turn_left(5);
-                    last_move = 0;
+                    env.last_move = 0;
                 }
 	        else {
                     drone.turn_right(5);
-                    last_move = 1;
+                    env.last_move = 1;
                 }
-                in_region = 1;
+                env.in_region = 1;
             }
             else {
-                if (drone.rssi > last_rssi) {
+                if (drone.rssi > env.last_rssi) {
                     // do nothing, keep going
                 }
                 else {
-                    if (last_move === 0) drone.turn_left(180-10*Math.random());
+                    if (env.last_move === 0) drone.turn_left(180-10*Math.random());
                     else drone.turn_right(180-10*Math.random());
                 }
             }
-            last_rssi = drone.rssi;
+            env.last_rssi = drone.rssi;
         }
 
         else {
-            in_region = 0;
+            env.in_region = 0;
             drone.acc = 200;
                 if (Math.random() > 0.5)
                     drone.turn_left(Math.random()*50);
